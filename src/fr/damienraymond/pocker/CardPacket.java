b5e9730e079e3248df234b65079d6c438a339e12
Damien RAYMOND <contact@damien-raymond.fr>
2015-10-03 14:10:28 +0200

    Add methods to generate the card packet
    - method to produce cards from cartesian product of level and color emuns
    - method to permute cards in order to shuffle to packet
    - method to generate card packet thanks to the two previous methods

--- src/fr/damienraymond/pocker/CardPacket.java
+++ src/fr/damienraymond/pocker/CardPacket.java
@@ -14,5 +14,44 @@ public class CardPacket {
     private Stream<Card> cards;
 
 
+    public CardPacket() {
+
+        this.cards = this.generateCardPacket();
+    }
+
+
+    public Stream<Card> generateCardPacket(){
+        List<Level> levels = new ArrayList<>(EnumSet.allOf(Level.class));
+        List<Color> colors = new ArrayList<>(EnumSet.allOf(Color.class));
+        List<Card> cards = this.cartesianProductToProduceCardPacket(levels, colors);
+
+        // Use of several permutation
+        int permutationNumber = 3;
+        for (int i = 0; i < permutationNumber; i++) {
+            cards = this.permutate(cards);
+        }
+
+        return cards.stream();
+    }
+
+    public List<Card> cartesianProductToProduceCardPacket(List<Level> levels, List<Color> colors) {
+        return levels.stream()
+                .flatMap(level ->
+                                colors.stream()
+                                        .map(color ->
+                                                        new Card(level, color)
+                                        )
+                ).collect(Collectors.toCollection(LinkedList::new));
+    }
+
+
+    public List<Card> permutate(List<Card> input) {
+        // Clone collection to improve immutability
+        ArrayList cards = (ArrayList)((ArrayList)input).clone();
+        Collections.shuffle(cards);
+        return cards;
+    }
+
+
 
 }
