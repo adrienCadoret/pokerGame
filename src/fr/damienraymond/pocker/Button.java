f3516b8418d990d50b64617052fc392e34387a68
Damien RAYMOND <contact@damien-raymond.fr>
2015-10-04 20:18:12 +0200

    Add method in Button to check if a player has the button and also to get the buttonOwnerPlayer

--- src/fr/damienraymond/pocker/Button.java
+++ src/fr/damienraymond/pocker/Button.java
@@ -4,4 +4,19 @@ package fr.damienraymond.pocker;
  * Created by damien on 01/10/2015.
  */
 public class Button {
+
+    private Player buttonOwnerPlayer;
+
+    /**
+     * This method is able to determine if a player is this who has the button
+     * @param p the player on want to check
+     * @return true if p has the button ; false otherwise
+     */
+    public boolean thisPlayerIsTheOwnerOfTheButton(Player p){
+        return (buttonOwnerPlayer != null) && buttonOwnerPlayer.equals(p);
+    }
+
+    public Player getButtonOwnerPlayer() {
+        return buttonOwnerPlayer;
+    }
 }
