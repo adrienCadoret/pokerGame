79a5425b9109abd1ac279de6b2e42378b823b997
Damien RAYMOND <contact@damien-raymond.fr>
2015-10-04 20:08:06 +0200

    Add getters, equals and hashCode methods

--- src/fr/damienraymond/pocker/Card.java
+++ src/fr/damienraymond/pocker/Card.java
@@ -13,5 +13,29 @@ public class Card {
         this.color = color;
     }
 
+    public Color getColor() {
+        return color;
+    }
 
+    public Level getLevel() {
+        return level;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        return getColor() == card.getColor() && getLevel() == card.getLevel();
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getColor().hashCode();
+        result = 31 * result + getLevel().hashCode();
+        return result;
+    }
 }
