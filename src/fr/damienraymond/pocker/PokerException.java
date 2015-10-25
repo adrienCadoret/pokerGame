b2095415c2159e295f518108b1fa9a16dcfdde89
Damien RAYMOND <contact@damien-raymond.fr>
2015-10-25 22:36:11 +0100

    Add a Poker exception

---
+++ src/fr/damienraymond/pocker/PokerException.java
@@ -0,0 +1,10 @@
+package fr.damienraymond.pocker;
+
+/**
+ * Created by damien on 25/10/2015.
+ */
+public class PokerException extends Exception {
+    public PokerException(String s) {
+        super(s);
+    }
+}
